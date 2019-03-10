75535e62e9b48c226ba88e7a4b1cd7ae950f3ea2
seyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-03-09 21:07:16 -1200

    Update AdsController.php

--- app/Http/Controllers/AdsController.php
+++ app/Http/Controllers/AdsController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 
 class AdsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('admin');
         $this->middleware('web');
 
     }
