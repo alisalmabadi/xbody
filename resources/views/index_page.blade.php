4c3f045154602c8c7db86acc4a5a8219f2461551
seyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-03-10 19:45:20 -1200

    Update index_page.blade.php

--- resources/views/index_page.blade.php
+++ resources/views/index_page.blade.php
@@ -1,5 +1,10 @@
     @extends('layouts.app')
+@section('head')
+    <style>
 
+
+    </style>
+    @endsection
     @section('main_content')
 
         @include('partials.sliders.slider_1')
@@ -11,5 +16,14 @@
         @include('partials.attr2')
         @include('partials.blog')
 
+@section('footer')
+    <script>
+        new WOW().init();
+    </script>
+    <script src="{{asset('js/jquery.paroller.min.js')}}"></script>
+    <script>
+        $(".row-container-first").paroller({ factor: 0.7, factorXs: 0.5, factorSm: 0.7, type: 'foreground', direction: 'vertical' , transition: 'transform 0.1s ease' });
 
+    </script>
+    @endsection
         @endsection
