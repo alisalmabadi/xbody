895589d39a1077d134e91340653b92c7fcd0afa7
seyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-03-10 19:46:15 -1200

    Update attr.blade.php

--- resources/views/partials/attr.blade.php
+++ resources/views/partials/attr.blade.php
@@ -1,40 +1,64 @@
+
+
 <!----------------------------- The Column Section -------------------->
 <div class="section-1">
-    <div class="row">
-        <div class="col-lg-4 col-md-12 text-center section-right-x">
-            <div class="section-right-xx">
-                <p class="col-12"> ویژگی های ایکس بادی ایران </p>
+    <div class="" style="width: 100%;height: 63px; position: absolute;overflow: visible; left:0px!important; ">
+        <div style="width: 100%;height: 100%; position: absolute;overflow: hidden !important;;">
+            <div class="transform" style="
+    right: 0;
+    width: 110%;
+    height: 100%;
+    -webkit-transform-origin:right;
+    transform-origin: right;
+    -webkit-transform: rotate(2deg);
+    transform: rotate(2deg);
+    position: absolute;background: white">
+
             </div>
         </div>
-        <div class="col-lg-8 col-md-12 text-center section-left-y">
-            <div class="col-12 section-left-ya">
-                <div class="col-lg-6 col-md-12 section-left-yaa">
-                    <p><i class="fa fa-chevron-left"></i> اولین شعب اصلی EMS در ایران </p>
-                    <h1>دارای بهترین فضای ورزشی در غرب تهران</h1>
+    </div>
+
+    <div class="row" id="reflection-info">
+        <div class="col-lg-5 col-md-12 text-center section-right-x">
+            <div class="reflection-box" style="margin-top: 18%;">
+        <div class="reflection-b">
+                <img class="image-responsive" src="images/reflection/reflection.png" style="width: 97%; position: absolute; right: 6px">
+            </div>
+                <div class="reflection-m">
+                    <img class="image-responsive" src="images/reflection/muscle.png" style="width:33%; height: 320px; position: absolute;right: 365px;top: 111px;">
                 </div>
-                <div class="col-lg-6 col-md-12 section-left-yab">
-                    <p><i class="fa fa-chevron-left"></i>  4 سال تجربه و 100% رضایت مشتریان </p>
-                    <h1>دارای بهترین فضای ورزشی در غرب تهران</h1>
+
+            </div>
+        </div>
+        <div class="col-lg-7 col-md-12 text-center section-left-y">
+            <div class="col-12 section-left-ya">
+                <div class="col-lg-6 col-md-12 section-left-yaa wow rollIn">
+                    <h1><i class="fa fa-chevron-left"></i> اولین شعب اصلی EMS در ایران </h1>
+                    <p>دارای بهترین فضای ورزشی در غرب تهران</p>
+                </div>
+                <div class="col-lg-6 col-md-12 section-left-yab wow rotateInUpRight">
+                    <h1><i class="fa fa-chevron-left"></i>  4 سال تجربه و 100% رضایت مشتریان </h1>
+                    <p>دارای بهترین فضای ورزشی در غرب تهران</p>
                 </div>
             </div>
             <div class="col-12 section-left-ya">
-                <div class="col-lg-6 col-md-12 section-left-yaa">
-                    <p><i class="fa fa-chevron-left"></i> جديدترين متد چربي سوزي و عضله سازي  </p>
-                    <h1>دارای بهترین فضای ورزشی در غرب تهران</h1>
+                <div class="col-lg-6 col-md-12 section-left-yaa wow bounceInUp">
+                    <h1><i class="fa fa-chevron-left"></i> جديدترين متد چربي سوزي و عضله سازي  </h1>
+                    <p>دارای بهترین فضای ورزشی در غرب تهران</p>
                 </div>
-                <div class="col-lg-6 col-md-12 section-left-yab">
-                    <p><i class="fa fa-chevron-left"></i> در 20 دقیقه 700 تا 1500 کالری بسوزانید </p>
-                    <h1>دارای بهترین فضای ورزشی در غرب تهران</h1>
+                <div class="col-lg-6 col-md-12 section-left-yab wow rotateOutDownLeft">
+                    <h1><i class="fa fa-chevron-left"></i> در 20 دقیقه 700 تا 1500 کالری بسوزانید </h1>
+                    <p>دارای بهترین فضای ورزشی در غرب تهران</p>
                 </div>
             </div>
             <div class="col-12 section-left-ya">
-                <div class="col-lg-6 col-md-12 section-left-yaa">
-                    <p><i class="fa fa-chevron-left"></i> تجربه راحت از کاهش وزن </p>
-                    <h1>دارای بهترین فضای ورزشی در غرب تهران</h1>
+                <div class="col-lg-6 col-md-12 section-left-yaa wow flipInX">
+                    <h1><i class="fa fa-chevron-left"></i> تجربه راحت از کاهش وزن </h1>
+                    <p>دارای بهترین فضای ورزشی در غرب تهران</p>
                 </div>
-                <div class="col-lg-6 col-md-12 section-left-yab" >
-                    <p><i class="fa fa-chevron-left"></i> کمتر از یکماه 5 کیلو کم کنید </p>
-                    <h1>دارای بهترین فضای ورزشی در غرب تهران</h1>
+                <div class="col-lg-6 col-md-12 section-left-yab wow rotateInDownRight" >
+                    <h1><i class="fa fa-chevron-left"></i> کمتر از یکماه 5 کیلو کم کنید </h1>
+                    <p>دارای بهترین فضای ورزشی در غرب تهران</p>
                 </div>
             </div>
         </div>
