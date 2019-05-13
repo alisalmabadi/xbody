071db959b24896d944731866628bac753480827d
reza <reza.sh_rs@yahoo.com>
2019-05-13 16:44:29 +0430

    Update Setting.php

--- app/Setting.php
+++ app/Setting.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    protected $fillable=['logo','facebook','instagram','telegram','product_header','product_des'];
+    protected $fillable=['logo','facebook','instagram','telegram','product_header','product_des','mainpage_header','mainpage_desc','galleryvideo_header','galleryvideo_des','galleryphoto_header','galleryphoto_des','gallerycustomer_header','gallerycustomer_des'];
 }
