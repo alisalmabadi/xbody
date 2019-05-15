fecac5504b9d23150d4403bde01951c4408cff38
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-05-15 18:52:22 +0430

    Update Menu.php

--- app/Menu.php
+++ app/Menu.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model
 {
-    protected $fillable=['name','link','parent_id','avatar'];
+    protected $fillable=['name','link','parent_id','avatar','type'];
 }
