77be0b241d7e19f059eeaf9beabd924da1d7a5ed
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-05-15 10:50:42 +0430

    Update Category.php

--- app/Category.php
+++ app/Category.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use SoftDeletes;
     protected  $fillable=['name','title','seo_desc','slug','image','skill','state'];
     public function keywords()
     {
