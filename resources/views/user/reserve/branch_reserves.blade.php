fa4a8270bc6091da705d0a3d9f2a3d87c350ca43
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-05-16 13:18:19 +0430

    Update branch_reserves.blade.php

--- resources/views/user/reserve/branch_reserves.blade.php
+++ resources/views/user/reserve/branch_reserves.blade.php
@@ -76,7 +76,7 @@
                                         <div class="modal-content">
                                             <div class="modal-header" style="background: #ff0000c7;">
                                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                <h4 class="modal-title" style="font-family: iransanse;">روز های رزروی</h4>
+                                                <h4 class="modal-title" style="font-family: iranyekan;">روز های رزروی</h4>
                                             </div>
                                             <div class="modal-body">
                                                 <table class="table table-striped border-top">
