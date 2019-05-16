2639141a4c643f9b25a84965604cd1c2a72aeec3
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-05-16 13:16:56 +0430

    Update Setting.php

--- app/Setting.php
+++ app/Setting.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    protected $fillable=['logo','facebook','instagram','telegram','product_header','product_des','mainpage_header','mainpage_desc','galleryvideo_header','galleryvideo_des','galleryphoto_header','galleryphoto_des','gallerycustomer_header','gallerycustomer_des','fabout','fcontactus'];
+    protected $fillable=['logo','facebook','instagram','telegram','product_header','product_des','mainpage_header','mainpage_desc','galleryvideo_header','galleryvideo_des','galleryphoto_header','galleryphoto_des','gallerycustomer_header','gallerycustomer_des','fabout','fcontactus','product_title','galleryvideo_title','galleryphoto_title','gallerycustomer_title','product_meta_desc'];
 }
