2c2dc87ea227bd4dad6e1b495366e0d81263d4e2
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-05-23 09:35:35 +0430

    Update index.blade.php

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -503,7 +503,7 @@
     padding: 5px;font-size:14px">تماس بگیرید</span>
                         @else
 
-                            {{Convertnumber2english($product->price)  }}  تومان
+                            {{Convertnumber2english(number_format($product->price))  }}  تومان
 
                         @endif
                     </h5>
